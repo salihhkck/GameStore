a3ea4404725cae77b5c69a812eb7defe2fc4ca4e
Salih Kucuk <salihkucukdeveloper@gmail.com>
2024-01-08 14:52:02 +0300

    Post, Put and Delete methods are added. Also, validation rules added in game entities.

--- GameStore.Api/Entitites/Game.cs
+++ GameStore.Api/Entitites/Game.cs
@@ -1,11 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Entitites;
 
 public class Game
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+
+    [Required]
+    [StringLength(30)]
     public required string Genre { get; set; }
+
+
+    [Range(1, 200)]
     public decimal Price { get; set; }
+
+    [Required]
     public DateTime ReleaseDate { get; set; }
+
+    [Required]
+    [Url]
+    [StringLength(100)]
     public required string ImageUri { get; set; }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,7 @@
 using GameStore.Api.Entitites;
 
+const string GetGameEndpointName = "GetGame";
+
 List<Game> games = new()
 {
     new Game()
@@ -34,6 +36,61 @@ List<Game> games = new()
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/games", () => games);
+var group = app.MapGroup("/games");
+
+group.MapGet("/", () => games);
+
+group.MapGet("/{id}", (int id) =>
+{
+    Game? game = games.Find(game => game.Id == id);
+
+    if (game is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
+
+group.MapPost("/", (Game game) =>
+{
+    game.Id = games.Max(game => game.Id) + 1;
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
+
+
+group.MapPut("/{id}", (int id, Game updatedGame) =>
+{
+    Game? existingGame = games.Find(game => game.Id == id);
+
+    if (existingGame is null)
+    {
+        return Results.NotFound();
+    }
+
+    existingGame.Name = updatedGame.Name;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ImageUri = updatedGame.ImageUri;
+    existingGame.Genre = updatedGame.Genre;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+
+    return Results.NoContent();
+});
+
+group.MapDelete("/{id}", (int id) =>
+{
+    Game? game = games.Find(game => game.Id == id);
+
+    if (game is not null)
+    {
+        games.Remove(game);
+    }
+
+    return Results.NoContent();
+});
+
 
 app.Run();
